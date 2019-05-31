95aee782d4c3c425b36e356559c288fb493726e0
Winnie Song <winniesong0121@gmail.com>
2019-05-31 13:53:01 -0400

    changed app title. attempting to add audio to app

---
+++ starwars/src/components/audio.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Sound from 'react-sound';
+ 
+class MyComponentWithSound extends React.Component {
+  render() {
+    return <Sound {...props} />; // Check props in next section
+  }
+}
